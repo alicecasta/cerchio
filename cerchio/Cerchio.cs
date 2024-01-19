9cb432f0718450b827a21d71b3cfef074f5cb83d
alice <castaldialicee@gmail.com>
2024-01-19 15:11:38 +0100

    Aggiungere i file di progetto.

---
+++ cerchio/Cerchio.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cerchio
+{
+    internal class Cerchio
+    {
+        public Cerchio()
+        {
+        }
+
+        public Cerchio(double raggio)
+        {
+            this.Raggio = raggio;
+        }
+
+        public double Raggio { get; set; } 
+        public double CalcoloArea()
+        {
+            return ((Raggio * Raggio * Math.PI));
+        }
+        public double CalcoloPerimetro()
+        {
+            return (Raggio * Math.PI * 2);
+        }
+    }
+}
---
+++ cerchio/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cerchio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cerchio c= new Cerchio();
+            Console.WriteLine("inserire raggio:");
+            c.Raggio = Double.Parse(Console.ReadLine());
+            Console.WriteLine( "l'area è {0}", c.CalcoloArea());
+            Console.WriteLine("il perimetro è {0}", c.CalcoloPerimetro());
+            Console.ReadLine();
+        }
+    }
+}
